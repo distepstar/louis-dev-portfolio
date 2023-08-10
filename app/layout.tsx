3c937e01242ccebc9fbb67fbea4061d2ce30c9ad
distepstar <jobsdzihinss@gmail.com>
2023-08-10 17:13:26 -0400

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "@/components";
 import "./globals.css";
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Louis Dev 🚀 - Frontend Developer",
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body className="bg-gradient-to-b from-slate-950 to-slate-700">
         <Navbar />
         {children}
+        <Analytics />
       </body>
     </html>
   );
